c0809b659812e617aaa44c73ff29ef5793d04936
StephenDunne97 <dunne134@gmail.com>
2023-04-21 23:11:54 +0100

    Add extra details and allow additional paramter when calling script

--- SnookerScripts/snooker.py
+++ SnookerScripts/snooker.py
@@ -11,12 +11,14 @@ colours = {
     "black":  7
     }
 
-def calc_max():
+def calc_max(detail="n"):
     total = 0
     for x in reds:
         total = total + (x + colours.get("black"))    
     total = total + calc_colours()
-    print(total)
+    print("Highest possible score:", total)
+    if detail != "n":
+        print("\nThis score is only possible if the player pots a black ball for every red ball they potted AND pots all colours in the final stage of the set.")
         
 def calc_colours():
     total = 0
@@ -25,4 +27,4 @@ def calc_colours():
     return total
 
 if __name__ == '__main__':
-    globals()[sys.argv[1]]()
+    globals()[sys.argv[1]](sys.argv[2])
